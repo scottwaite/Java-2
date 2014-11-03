580886890233944498a99fadc662889c03b48b15
scottwaite <scottwaite@MACBOOKPRO-CB99.home>
2014-11-02 21:07:25 -0500

    revert

--- Week 1/Practice/app/src/main/java/com/fullsail/android/showtimes/fragment/ShowtimeListFragment.java
+++ Week 1/Practice/app/src/main/java/com/fullsail/android/showtimes/fragment/ShowtimeListFragment.java
@@ -12,26 +12,26 @@ import com.scottwaite.android.showtimes.R;
 public class ShowtimeListFragment extends ListFragment {
 	
 	public static final String TAG = "ShowtimeListFragment.TAG";
-
+	
 	public static ShowtimeListFragment newInstance() {
 		ShowtimeListFragment frag = new ShowtimeListFragment();
 		return frag;
 	}
-
+	
 	@Override
 	public void onActivityCreated(Bundle _savedInstanceState) {
 		super.onActivityCreated(_savedInstanceState);
-
+		
 		String[] showtimes = getResources().getStringArray(R.array.showtimes);
-
+		
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, showtimes);
 		setListAdapter(adapter);
 	}
-
+	
 	@Override
 	public void onListItemClick(ListView _l, View _v, int _position, long _id) {
 		String showtime = (String)_l.getItemAtPosition(_position);
-
+		
 		new AlertDialog.Builder(getActivity())
 		.setTitle(R.string.showtime)
 		.setMessage(getString(R.string.selected, showtime))
