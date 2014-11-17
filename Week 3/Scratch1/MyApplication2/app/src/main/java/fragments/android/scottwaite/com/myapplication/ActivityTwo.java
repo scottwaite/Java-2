1f3c87e2b78c4c4b696fb62290a190ea6fe006a5
scottwaite <scottwaite@MACBOOKPRO-CB99.home>
2014-11-17 00:05:43 -0500

    Fixed Submission for Week 3

---
+++ Week 3/Scratch1/MyApplication2/app/src/main/java/fragments/android/scottwaite/com/myapplication/ActivityTwo.java
@@ -0,0 +1,55 @@
+package fragments.android.scottwaite.com.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class ActivityTwo  extends Activity {
+
+    private WebView webView;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+
+        webView = (WebView) findViewById(R.id.webView1);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://photos-h.ak.instagram.com/hphotos-ak-xfa1/10735367_343156522522687_148296656_n.jpg");
+
+        String customHtml = "<html><body><div data-role=\"page\" data-control-title=\"Home\" id=\"page1\">\n" +
+                "    <div role=\"main\" class=\"ui-content\">\n" +
+                "        <div style=\"\" data-controltype=\"image\">\n" +
+                "            <img style=\"width: 288px; height: 200px\" src=\"http://photos-a.ak.instagram.com/hphotos-ak-xfa1/10727555_470602719749416_1497996850_n.jpg\">\n" +
+                "        </div>\n" +
+                "        <div style=\"\" data-controltype=\"image\">\n" +
+                "            <img style=\"width: 288px; height: 200px\" src=\"http://photos-h.ak.instagram.com/hphotos-ak-xfa1/10735367_343156522522687_148296656_n.jpg\">\n" +
+                "        </div>\n" +
+                "        <div style=\"\" data-controltype=\"image\">\n" +
+                "            <img style=\"width: 288px; height: 200px\" src=\"http://photos-g.ak.instagram.com/hphotos-ak-xfa1/10724129_743658195715158_355281060_n.jpg\">\n" +
+                "        </div>\n" +
+                "        <div style=\"\" data-controltype=\"image\">\n" +
+                "            <img style=\"width: 288px; height: 200px\" src=\"http://photos-d.ak.instagram.com/hphotos-ak-xfa1/10727555_792630690783891_730766625_n.jpg\">\n" +
+                "        </div>\n" +
+                "        <div style=\"\" data-controltype=\"image\">\n" +
+                "            <img style=\"width: 288px; height: 200px\" src=\"http://photos-h.ak.instagram.com/hphotos-ak-xaf1/10727283_770851142988631_655788802_n.jpg\">\n" +
+                "        </div>\n" +
+                "        <div style=\"\" data-controltype=\"image\">\n" +
+                "            <img style=\"width: 288px; height: 200px\" src=\"http://photos-e.ak.instagram.com/hphotos-ak-xfa1/10727822_697379977045092_655073191_n.jpg\">\n" +
+                "        </div>\n" +
+                "        <div style=\"\" data-controltype=\"image\">\n" +
+                "            <img style=\"width: 288px; height: 200px\" src=\"http://photos-h.ak.instagram.com/hphotos-ak-xaf1/10731661_724072007646575_867228503_n.jpg\">\n" +
+                "        </div>\n" +
+                "        <div>\n" +
+                "            <a href=\"http://instagram.com/snoopygrams\" data-transition=\"fade\">\n" +
+                "                Profile\n" +
+                "            </a>\n" +
+                "        </div>\n" +
+                "    </div>\n" +
+                "</div></body></html>";
+        webView.loadData(customHtml, "text/html", "UTF-8");
+
+    }
+
+}
---
+++ Week 3/Scratch1/MyApplication2/app/src/main/java/fragments/android/scottwaite/com/myapplication/MainActivity.java
@@ -0,0 +1,204 @@
+package fragments.android.scottwaite.com.myapplication;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.ContentProviderOperation;
+import android.content.Intent;
+import android.content.OperationApplicationException;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.provider.ContactsContract.CommonDataKinds.Photo;
+import android.provider.ContactsContract.CommonDataKinds.StructuredName;
+import android.provider.ContactsContract.RawContacts;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    private final int PICK_PHOTO = 1;
+
+    Bitmap mBitmap;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        OnClickListener photoClickListener = new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, PICK_PHOTO);
+            }
+        };
+
+        OnClickListener addClickListener = new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                EditText etName = (EditText) findViewById(R.id.et_name);
+
+                EditText etMobile = (EditText) findViewById(R.id.et_mobile);
+
+                ArrayList<ContentProviderOperation> ops =
+                        new ArrayList<ContentProviderOperation>();
+
+                int rawContactID = ops.size();
+
+                ops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                        .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                        .withValue(RawContacts.ACCOUNT_NAME, null)
+                        .build());
+
+                ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactID)
+                        .withValue(ContactsContract.Data.MIMETYPE, StructuredName.CONTENT_ITEM_TYPE)
+                        .withValue(StructuredName.DISPLAY_NAME, etName.getText().toString())
+                        .build());
+
+                ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactID)
+                        .withValue(ContactsContract.Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE)
+                        .withValue(Phone.NUMBER, etMobile.getText().toString())
+                        .withValue(Phone.TYPE, CommonDataKinds.Phone.TYPE_MOBILE)
+                        .build());
+
+
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                if(mBitmap!=null){
+                    mBitmap.compress(Bitmap.CompressFormat.PNG , 75, stream);
+
+                    ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                            .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactID)
+                            .withValue(ContactsContract.Data.IS_SUPER_PRIMARY, 1)
+                            .withValue(ContactsContract.Data.MIMETYPE,Photo.CONTENT_ITEM_TYPE)
+                            .withValue(ContactsContract.CommonDataKinds.Photo.PHOTO,stream.toByteArray())
+                            .build());
+
+                    try {
+                        stream.flush();
+                    }catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try{
+                    getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+                    Toast.makeText(getBaseContext(), "Contact saved to device", Toast.LENGTH_SHORT).show();
+                }catch (RemoteException e) {
+                    e.printStackTrace();
+                }catch (OperationApplicationException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+
+        OnClickListener contactsClickListener = new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent contacts = new Intent(Intent.ACTION_VIEW,ContactsContract.Contacts.CONTENT_URI);
+
+                startActivity(contacts);
+            }
+        };
+
+
+
+        ImageButton ibPhoto = (ImageButton) findViewById(R.id.ib_photo);
+
+        Button btnAdd = (Button) findViewById(R.id.btn_add);
+
+        Button btnList = (Button) findViewById(R.id.btn_list);
+
+        ibPhoto.setOnClickListener(photoClickListener);
+
+        btnAdd.setOnClickListener(addClickListener);
+
+
+        btnList.setOnClickListener(contactsClickListener);
+
+
+        Button btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(activityClickListener);
+
+
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode) {
+            case PICK_PHOTO:
+                if(resultCode == RESULT_OK){
+                    // Getting the uri of the picked photo
+                    Uri selectedImage = data.getData();
+
+                    InputStream imageStream = null;
+                    try {
+                        // Getting InputStream of the selected image
+                        imageStream = getContentResolver().openInputStream(selectedImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+                    // Creating bitmap of the selected image from its inputstream
+                    mBitmap = BitmapFactory.decodeStream(imageStream);
+
+                    // Getting reference to ImageView
+                    ImageButton ibPhoto = (ImageButton) findViewById(R.id.ib_photo);
+
+                    // Setting Bitmap to ImageButton
+                    ibPhoto.setImageBitmap(mBitmap);
+                }
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+
+
+
+
+
+        OnClickListener activityClickListener = new OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                Intent intent = new Intent("fragments.android.scottwaite.ActivityTwo");
+                startActivity(intent);
+            }
+        };
+
+
+
+}
+
+
